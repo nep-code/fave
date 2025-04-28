06ada9d78cd1d7abf95c13e6fa53f4d87e739f2e
Neptali Calonge <36485332+nep-code@users.noreply.github.com>
2025-04-28 19:08:41 +0800

    Add files via upload

--- logic.js
+++ logic.js
@@ -5,15 +5,19 @@ var container	= document.getElementById("container"),
     is_mobile	= ('ontouchstart' in document.documentElement && /mobi/i.test(navigator.userAgent)),
     is_chrome   = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor),
     z0          = is_chrome ? 0 : 0.01,
+    array       = [],
+    count       = 2,
 
 INIT = (data) => {
-    ADD_HEADLINE(data.surveyDescription);
+    ADD_HEADLINE(data.headline);
 
-    const options = data.cards.options;
-    options.forEach((opt, index) => {
-        ADD_CARD(opt.optionId, opt.imageUrl, opt.description);
-        if (index !== options.length - 1) ADD_OR();
-    });
+    array = SHUFFLE(data.options);
+
+    for (let i=0; i < count; i++) {
+        ADD_CARD(i, array[0].image, array[0].name);
+        array.shift();
+        if (i !== count - 1) ADD_OR();
+    }
 
     ANIMATE();
 },
@@ -25,20 +29,69 @@ ADD_HEADLINE = (data) => {
     container.prepend(headline);
 },
 
-ADD_CARD = (id,img,txt) => {
-    let card   = document.createElement("div"),
+SHUFFLE = (arr) => {
+    arr.forEach((data, i) => {
+        const n = Math.floor(Math.random() * arr.length);
+        [arr[i], arr[n]] = [arr[n], arr[i]];
+    });
+    return arr;
+},
+
+ADD_CARD = (num,img,txt) => {
+    let card    = document.createElement("div"),
         desc    = document.createElement("div");
 
-    card.id = id;
+    card.id = `card_${num}`;
     card.className = "card";
     card.style.backgroundImage = `url(${img})`;
+    card.style.cursor = "pointer";
 
     desc.className = "desc";
     desc.innerHTML = txt;
 
+    PRELOADIMAGE(img);
+
     card.append(desc);
-    
+
+    card.addEventListener("click", ()=> {
+        switch(card.id){
+            case "card_0":
+                gsap.to("#card_1", {duration:0.5, rotateY:90, scale:0.8, skewY:6, z:z0, ease:"power1.out", onComplete: ()=> SWAP_CARD("card_1")});
+                break;
+            case "card_1": 
+                gsap.to("#card_0", {duration:0.5, rotateY:90, scale:0.8, skewY:6, z:z0, ease:"power1.out", onComplete: ()=> SWAP_CARD("card_0")});
+                break;
+        }
+    });
+
     content.append(card);
+
+},
+
+PRELOADIMAGE = (src) => {
+    const img = new Image();
+    img.src = src;
+},
+
+SWAP_CARD = (id) => {
+    let card    = document.getElementById(id),
+        cards   = content.querySelector(".card"),
+        desc    = card.querySelector(".desc"),
+        or    = content.querySelector(".or")
+
+    console.log(array.length, array);
+    if(array.length === 0) {
+        card.style.display = "none";
+        or.style.display = "none";
+        cards.style.cursor = "auto";
+        cards.removeEventListener("click");
+    } else {   
+        card.style.backgroundImage = `url(${array[0].image})`;
+        desc.innerHTML = array[0].name;
+        array.shift();
+        gsap.fromTo(card, {skewY:-6},{duration:0.5, delay:0.1, scale:1, rotateY:0, skewY:0, z:z0, ease:"power1.inOut"});
+        PRELOADIMAGE(array[0].image);
+    }
 },
 
 ADD_OR = () => {
@@ -61,7 +114,7 @@ ANIMATE = () => {
     window.duration = tl.totalDuration();
 };
 
-fetch('answers.json')
+fetch('src/categories/sports/basketball/NBA_001.json')
 .then(response => {
     if (!response.ok) throw new Error('Network Response Error');
     return response.json();
